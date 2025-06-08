ab5bc2faae8012528e049a732908d9808c91e292
bliss <107047190+Blisse1@users.noreply.github.com>
2025-06-07 23:49:24 -0500

    filter working

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,79 @@
+import { useState } from 'react';
+import { gql, useQuery } from '@apollo/client'
+import Character from './components/Character';
 import './App.css'
 import API from './components/API'
 
+const RICK_INFO = gql`
+    query {
+        characters{
+    info {
+      count
+    }
+    results {
+      id
+      name
+      gender
+      status
+      origin{
+        name 
+      }
+      image
+    }
+  }
+      } 
+    `;
+
 function App() {
-  return (
-      <>
-      <form action="">
-      <select name="" id="">
-        <option value="female">female</option>
-        <option value="male">male</option>
-      </select>
-        <button type='submit'>Submit</button>
-      </form>
-    <API />
-    </>
-  );
+    const [characters, setCharacters] = useState([]);
+    const { loading, error, data } = useQuery(RICK_INFO);
+
+    if (loading) return <p>Loading...</p>
+    if (error) return <p>Error: {error.message}</p>
+
+    const myData = data.characters.results;
+
+    function handleSubmit(e){
+        e.preventDefault();
+        const optionValue = e.target.elements.selectedGender.value;
+        const filterCharacters = myData.filter(character => character.gender === optionValue);
+
+        const filter = filterCharacters.map(({ id, name, origin, image, status, gender }) => {
+            const myObj = {
+                id,
+                name,
+                origin: origin.name,
+                image,
+                gender,
+                status
+            };
+            return (
+                <>
+                <li key={id}>
+                    <Character {...myObj} />
+                </li>
+                </>
+            );
+        })
+
+        setCharacters(filter);
+    }
+
+    return (
+        <>
+        <form onSubmit={handleSubmit}>
+            <select name='selectedGender'>
+                <option value="">select</option>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+            </select>
+            <button>Filter gender</button>        
+        </form>
+        <ul>
+        {characters}
+        </ul>
+        </>
+    );
 }
 
 export default App
--- src/components/API.jsx
+++ src/components/API.jsx
@@ -1,68 +1,8 @@
 import { gql, useQuery } from '@apollo/client'
 import Character from "./Character"
-
-const RICK_INFO = gql`
-    query {
-        characters{
-    info {
-      count
-    }
-    results {
-      id
-      name
-      gender
-      status
-      origin{
-        name 
-      }
-      image
-    }
-  }
-      } 
-    `;
+import { useState } from 'react';
 
 function Data() {
-  const { loading, error, data } = useQuery(RICK_INFO);
-  if (loading) return <p>Loading...</p>
-  if (error) return <p>Error: {error.message}</p>
-
-  // console.log(myFilter);
-
-  // recomendaciones
-  // no mezclar jsx
-  // el bloque de return con el jsx
-  // un objeto representacion por prop y mandarlo como spread
-
-  // agrupar componentes por responsabilidades usando 
-
-  const myData = data.characters.results;
-
-  const femalesFilter = myData.filter(character => character.gender === "Female");
-
-  const malesFilter = myData.filter(character => character.gender === "male");
-
-  // const mapFemales = femalesFilter.map(({ id, name, origin, image, status }) => (
-  //   <li key={id}><Character key={id} name={name} origin={origin.name} image={image} status={status} /></li>
-  // ));
-
-  const myCharacters = femalesFilter.map(({ id, name, origin, image, status }) => {
-    const myObj = {
-      name,
-      origin: origin.name,
-      image,
-      status,
-    };
-
-    return (
-      <>
-      <li key={id}>
-        <Character {...myObj} />
-      </li>
-      </>
-    );
-  });
-
-  return myCharacters;
 }
 
 export default Data;
